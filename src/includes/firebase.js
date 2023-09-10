c9071710983d2dce1710f50a06dca00d6c03cdf7
Fly0w <florian.budniewski@hotmail.com>
2023-09-10 12:04:30 +0900

    added env variable

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -12,15 +12,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID
 }
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyD5breK_Vk9VV7AHQEeN8_tQv-9mHyrWIw',
-//   authDomain: 'sharehouse-96ee0.firebaseapp.com',
-//   projectId: 'sharehouse-96ee0',
-//   storageBucket: 'sharehouse-96ee0.appspot.com',
-//   messagingSenderId: '172112341807',
-//   appId: '1:172112341807:web:1718f46cc281c56a36cfda'
-// }
-
 // firebase.initializeApp(firebaseConfig)
 const app = initializeApp(firebaseConfig)
 
