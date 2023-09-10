9f8152e3b509f6f94d1a6f6f8afca6029ced855c
Fly0w <florian.budniewski@hotmail.com>
2023-09-10 12:04:04 +0900

    added env variable

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -3,23 +3,23 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.API_KEY,
-//   authDomain: process.env.AUTH_DOMAIN,
-//   projectId: process.env.PROJECT_ID,
-//   storageBucket: process.env.STORAGE_BUCKET,
-//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-//   appId: process.env.APP_ID
-// }
-
 const firebaseConfig = {
-  apiKey: 'AIzaSyD5breK_Vk9VV7AHQEeN8_tQv-9mHyrWIw',
-  authDomain: 'sharehouse-96ee0.firebaseapp.com',
-  projectId: 'sharehouse-96ee0',
-  storageBucket: 'sharehouse-96ee0.appspot.com',
-  messagingSenderId: '172112341807',
-  appId: '1:172112341807:web:1718f46cc281c56a36cfda'
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 }
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyD5breK_Vk9VV7AHQEeN8_tQv-9mHyrWIw',
+//   authDomain: 'sharehouse-96ee0.firebaseapp.com',
+//   projectId: 'sharehouse-96ee0',
+//   storageBucket: 'sharehouse-96ee0.appspot.com',
+//   messagingSenderId: '172112341807',
+//   appId: '1:172112341807:web:1718f46cc281c56a36cfda'
+// }
 
 // firebase.initializeApp(firebaseConfig)
 const app = initializeApp(firebaseConfig)
